bf9528b93f2c2d120077cf4d816290c4ea47a472
KokilaSivasankar <157289599+KokilaSivasankar@users.noreply.github.com>
2024-01-25 21:17:52 +0530

    Create reverseanumber.cpp

---
+++ 22-01-2024/reverseanumber.cpp
@@ -0,0 +1,54 @@
+*/Given a number N reverse the number and print it.
+
+Example 1: Input: N = 123 Output: 321 Explanation: The reverse of 123 is 321
+
+Example 2: Input: N = 234 Output: 432 Explanation: The reverse of 234 is 432
+
+Input Format
+
+123
+
+Constraints
+
+N <= 1000
+
+Output Format
+
+321
+
+Sample Input 0
+
+123
+Sample Output 0
+
+321 */
+
+
+
+
+
+
+
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin>>n;
+    int r;
+    while(n!=0)
+    {
+        int temp = n%10;
+        r= temp*10 +temp;
+        n /=10;
+    }
+    cout<<r;
+    return 0;
+}
