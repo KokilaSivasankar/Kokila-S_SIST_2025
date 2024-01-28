b7a1a128043afdb1ce8887127ac32bff0d2a02a9
KokilaSivasankar <157289599+KokilaSivasankar@users.noreply.github.com>
2024-01-28 21:13:48 +0530

    Create anagram.py

---
+++ 27-01-2024/anagram.py
@@ -0,0 +1,84 @@
+'''Two words are anagrams of one another if their letters can be rearranged to form the other word.
+
+Given a string, split it into two contiguous substrings of equal length. Determine the minimum number of characters to change to make the two substrings into anagrams of one another.
+
+Example
+
+Break  into two parts: 'abc' and 'cde'. Note that all letters have been used, the substrings are contiguous and their lengths are equal. Now you can change 'a' and 'b' in the first substring to 'd' and 'e' to have 'dec' and 'cde' which are anagrams. Two changes were necessary.
+
+Function Description
+
+Complete the anagram function in the editor below.
+
+anagram has the following parameter(s):
+
+string s: a string
+Returns
+
+int: the minimum number of characters to change or -1.
+Input Format
+
+The first line will contain an integer, , the number of test cases.
+Each test case will contain a string .
+
+Constraints
+
+
+ consists only of characters in the range ascii[a-z].
+Sample Input
+
+6
+aaabbb
+ab
+abc
+mnop
+xyyx
+xaxbbbxx
+Sample Output
+
+3
+1
+-1
+2
+0
+1'''
+
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'anagram' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def anagram(s):
+    if len(s) % 2 == 1:
+        return -1
+
+    s1 = s[:int(len(s) / 2)]
+    s2 = s[int(len(s) / 2):]
+    unique_chars = list(set(s2))
+    count = 0
+    for c in unique_chars:
+        count += max(s2.count(c) - s1.count(c), 0)
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = anagram(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
