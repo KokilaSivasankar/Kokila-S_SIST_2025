ea7a1d985b404f640cbdc89a1e884c1e97d35539
KokilaSivasankar <157289599+KokilaSivasankar@users.noreply.github.com>
2024-01-30 21:48:45 +0530

    Update and rename palindrome.cpp to besttimetosellandbuy.cpp

---
+++ 30-01-2024/besttimetosellandbuy.cpp
@@ -0,0 +1,41 @@
+/**You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+ **/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.size() == 0) return 0;
+        
+        int bs = 0;
+        
+        int start = prices[0], end = prices[0];
+        
+        for(int i = 0; i < prices.size(); i++){
+            if(prices[i] < start){
+                //restart as session
+                bs = max(bs, end - start);
+                start = prices[i];
+                end = prices[i];
+            }else{
+                //continue current session
+                end = max(end, prices[i]);
+            }
+        }
+        bs = max(bs, end - start);
+        return bs;
+    }
+};
+
